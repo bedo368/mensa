47b34e81e35db0561433d88d46ad73f1aaa370f6
bedo368 <76117903+bedo368@users.noreply.github.com>
2024-12-22 14:42:38 +0200

    fffffffffffffffffffffffffffffffffff

--- pages/test/[id].js
+++ pages/test/[id].js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import jwtDecode from "jsonwebtoken";
 import Lottie from "react-lottie";
-import starAnimation from "../../public/images/ani1.json"; // Replace with your actual path
+import starAnimation from "../../public/images/ani1.json";
 
 export default function StudentTest() {
   const router = useRouter();
